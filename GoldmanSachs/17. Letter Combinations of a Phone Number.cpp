2150d90fe85a03b4deb27f2ba3781139b8865ed1
Gyanthakur <gps.96169@gmail.com>
2024-01-05 11:15:08 +0530

    Combinations of a Phone Number

---
+++ GoldmanSachs/17. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,35 @@
+/* https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    void solve(string digits, string output,int index,vector<string> & ans,string mapping[])
+    {
+        if(index>=digits.length())
+        {
+            ans.push_back(output);
+            return;
+        }
+        int number = digits[index]-'0';
+        // string value = mapping[number];
+        string value=mapping[number];
+        for(int i=0;i<value.length();i++)
+        {
+            output.push_back(value[i]);
+            solve(digits,output,index+1,ans,mapping);
+            output.pop_back();
+        }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string>ans;
+        if(digits.length()==0)
+            return ans;
+        string output;
+        int index=0;
+        string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits,output,index,ans,mapping);
+        return ans;
+    }
+};
