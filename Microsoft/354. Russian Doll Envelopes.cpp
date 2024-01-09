951ca428b339083f94de4360c0d62d644c29b71c
Gyanthakur <gps.96169@gmail.com>
2024-01-09 17:09:41 +0530

    Russian Doll Envelopes

---
+++ Microsoft/354. Russian Doll Envelopes.cpp
@@ -0,0 +1,49 @@
+/* https://leetcode.com/problems/russian-doll-envelopes/ */
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    static bool cmp(vector<int>& a, vector<int>& b){
+        if(a[0]==b[0]) return a[1] > b[1];
+        return a[0] < b[0];
+    }
+public:
+    int maxEnvelopes(vector<vector<int>>& env) {
+        int n = env.size();
+        
+        // sorting by height & if we encounter same height
+        // sort by descending order of width
+        sort(env.begin(), env.end(), cmp);
+        
+        // e.g. -> env => (3,8) (4,5) (2,1) (2,6) (7,8) (5,3) (5,7)
+        // sorted version => (2,6) (2,1) (3,8) (4,5) (5,7) (5,3) (7,8)
+        
+        // Now, we only need to care about width
+        // So, as of now we only need to look upon v[i][1]
+        // [ 6, 1, 8, 5, 7, 3, 8 ]
+        
+        // Now, we can only put envolop a in envolop b another if width of a is
+        // less than width of b, or we can say we need an envolop whose width
+        // is just greater than envolop a ( height we have already handled )
+        // So, we can think of lower_bound and Longest Increasing Subsequence
+        
+        // because we have sorted all envolopes of a particular height
+        // by descending order of width, one envolope will not interfare with 
+        // another envolop of same height, if we apply lower_bound
+        // i.e. first elem greater than equal to itself in lis
+        
+       vector<int>ans;
+       for(int i=0;i<n;i++)
+       {
+           int ele = env[i][1];
+           int index = lower_bound(ans.begin(),ans.end(),ele)-ans.begin();
+           if(index>=ans.size())
+           ans.push_back(ele);
+           else
+           ans[index]=ele;
+       }
+       return ans.size();
+    }
+};
