3a0829f3fa458c624d30318e0352266e54b21ba8
Gyanthakur <gps.96169@gmail.com>
2024-01-09 17:06:42 +0530

    Find the Winner of the Circular Game

---
+++ Microsoft/1823. Find the Winner of the Circular Game.cpp
@@ -0,0 +1,15 @@
+/* https://leetcode.com/problems/find-the-winner-of-the-circular-game/description/ */
+
+class Solution {
+public:
+    int solve(int n, int k)
+    {
+        if(n == 1)return 0;
+        return (solve(n-1, k) + k)%n;
+    }
+    int findTheWinner(int n, int k) {
+        
+        int ans = solve(n,k);
+        return ans+1;
+    }
+};
