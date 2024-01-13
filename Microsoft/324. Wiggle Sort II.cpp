d2b97b11b231df05cb37646b85a43e30657dbf01
Gyanthakur <gps.96169@gmail.com>
2024-01-13 11:25:52 +0530

    Wiggle Sort II

--- Microsoft/324. Wiggle Sort II.cpp
+++ Microsoft/324. Wiggle Sort II.cpp
@@ -6,15 +6,21 @@ using namespace std;
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
+        sort(begin(nums),end(nums));
         int n = nums.size();
-        priority_queue<int> q(nums.begin(), nums.end());
-        for(int i=1; i<n; i+=2) {
-            nums[i] = q.top();
-            q.pop();
+        vector<int>v(n);
+        int j = n-1;
+        for(int i=1;i<n;i+=2)
+        {
+            v[i] = nums[j--];
         }
-        for(int i=0; i<n; i+=2) {
-            nums[i] = q.top();
-            q.pop();
+        for(int i = 0;i<n-1;i+=2)
+        {
+            v[i] = nums[j--];
+        }
+        for(int i=0 ; i<n;i++)
+        {
+            nums[i] = v[i];
         }
     }
 };
