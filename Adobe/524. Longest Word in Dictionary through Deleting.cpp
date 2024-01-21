13aff60c6901e5d2f9e5ada68151ee96a07e93d2
Gyanthakur <gps.96169@gmail.com>
2024-01-21 12:08:28 +0530

    Longest Word in Dictionary through Deleting

---
+++ Adobe/524. Longest Word in Dictionary through Deleting.cpp
@@ -0,0 +1,30 @@
+/* https://leetcode.com/problems/longest-word-in-dictionary-through-deleting/description/ */
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+      bool isSubsequence(string sub, string super){
+        int i = 0, j = 0;
+        while(i < sub.size() && j < super.size()){
+            if(sub[i] == super[j]){
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return i == sub.size();
+    };
+    
+    string findLongestWord(string s, vector<string>& d) {
+        string ans = "";
+        for(string& word : d){
+            if(word.size() > ans.size() || (word.size() == ans.size() && word < ans)){
+                if(isSubsequence(word, s)){
+                    ans = word;
+                }
+            }
+        }
+        return ans;
+    }
+};
